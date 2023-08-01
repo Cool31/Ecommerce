1ded4b29bdc3e93dd330f1c932c9ae44425e6633
Suresh S <coolvivel31@gmail.com>
2023-08-01 12:33:02 +0530

    Add files via upload

---
+++ postingformdata.js
@@ -0,0 +1,11 @@
+ fetch("https://www.github.com/cool31/cool31/main/financial/outputdata.php",{
+ method:'POST',
+ body: new FormData(document.getElementById("Bulk"))
+});
+
+<!-- sending cookies-->
+fetch("https://www.bambora.com/.../endpoint2/login",{
+Credentials:'include'
+});
+
+ 
