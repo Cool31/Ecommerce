9bfb64d6cf8668dc698a4ab70bc94558f59ddedb
Suresh S <coolvivel31@gmail.com>
2023-08-01 12:37:10 +0530

    Add files via upload

---
+++ refund.php
@@ -0,0 +1,93 @@
+<?php 
+require 'vendor/autoload.php'; 
+require_once 'SampleCode/Constants/indexind.php'; 
+
+use net\authorize\api\contract\v1 as 
+AnetAPI; 
+ use net\authorize\api\controller as 
+AnetController; 
+  
+define("AUTHORIZENET_LOG_FILE", "phplog"); 
+  function refundTransaction($refTransId, $amount) 
+ { 
+/* Create a merchantAuthenticationType object with authentication details 
+retrieved from the constants file */ 
+
+//To obtain the API Login ID and Transaction Key:
+Log into the Merchant Interface.
+Click Account from the main toolbar.
+Click Settings in the main left-side menu.
+Click API Credentials & Keys.
+Select New Transaction Key.
+ $merchantAuthentication = new AnetAPI\MerchantAuthenticationType(); 
+ $merchantAuthentication->setName(\SampleCode\Constants::MERCHANT_LOGIN_ID); 
+
+$merchantAuthentication->setTransactionKey(\SampleCode\Constants::MERCHANT_TRANSACTION_KEY); 
+      
+// Set the transaction's refId 
+ $refId='ref'.time(); 
+  
+// Create the payment data for a credit card 
+ $creditCard = new AnetAPI\CreditCardType(); $creditCard->setCardNumber("0015"); 
+$creditCard->setExpirationDate("XXXX"); 
+
+$paymentOne = 
+new AnetAPI\PaymentType(); 
+ $paymentOne->setCreditCard($creditCard); 
+ 
+ //create a transaction 
+ $transactionRequest = new AnetAPI\TransactionRequestType(); 
+ $transactionRequest->setTransactionType( "refundTransaction");  $transactionRequest->setAmount($amount); 
+$transactionRequest->setPayment($paymentOne); 
+$transactionRequest->\CreateTransactionRequest(); 
+$request->setMerchantAuthentication($merchantAuthentication); 
+$request->setRefId($refId); 
+$request->setTransactionRequest( $transactionRequest); 
+ $controller = new AnetController\CreateTransactionController($request); 
+$response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX); 
+  
+if($response != null) 
+{ 
+if($response->getMessages()->getResultCode() == "Ok") 
+ { 
+$tresponse = $response->getTransactionResponse(); 
+if($tresponse != null && $tresponse->getMessages() != null)    
+  { 
+echo "Transaction Response code:" . $tresponse->getResponseCode() . "\n"; 
+ echo "Refund SUCCESS: " . $tresponse->getTransId() . "\n"; 
+ echo " Code : " . $tresponse->getMessages()[0]->getCode() . "\n";  
+ echo " Description : " . $tresponse->getMessages()[0]->getDescription() . "\n"; 
+  } 
+  else 
+   { 
+echo "Transaction Failed \n"; 
+ if($tresponse->getErrors() != null) 
+  { 
+echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n"; 
+ echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";              } 
+ } 
+} 
+ else 
+ { 
+echo "Transaction Failed \n"; 
+$tresponse = $response->getTransactionResponse(); 
+
+if($tresponse != null && $tresponse->getErrors() != null) 
+ { 
+echo " Error code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n"; 
+echo " Error message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";            } 
+else 
+{ 
+echo " Error code  : " . $response->getMessages()->getMessage()[0]->getCode() . "\n"; 
+echo " Error message : " . $response->getMessages()->getMessage()[0]->getText() . "\n"; 
+    } 
+  }       
+} 
+else 
+{ 
+echo "No response returned \n"; 
+ } 
+return $response; 
+ } 
+if(!defined('DONT_RUN_SAMPLES')) 
+refundTransaction( "2.23");
