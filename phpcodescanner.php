ad25c1935858e30083a319b789154aa148b3b422
Suresh S <coolvivel31@gmail.com>
2023-08-03 16:42:21 +0530

    Update phpcodescanner.php
    to update link on folder creation
    creation

--- phpcodescanner.php
+++ phpcodescanner.php
@@ -1,5 +1,5 @@
 <?php
-//install AMWScan library
-use AMWScan\Scanner; $app = new Scanner();
-$report = $app->setPathScan("https://github.com/cool31/cool31/main/financial/phpcodescanner") ->enableBackups() ->setPathBackups("("https://github.com/cool31/cool31/main/financial/phpcodescanner/backups") ->enableLiteMode() ->setAutoClean() ->run();
+use AMWScan\Scanner; 
+$app = new Scanner();
+$report = $app->setPathScan("  ") ->enableBackups() ->setPathBackups("("/.../backups") ->enableLiteMode() ->setAutoClean() ->run();
 ?>
