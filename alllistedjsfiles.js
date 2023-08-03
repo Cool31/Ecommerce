acd0906a13a87c663f335bf5be22db135869f26e
Suresh S <coolvivel31@gmail.com>
2023-08-03 18:43:28 +0530

    Add files via upload

---
+++ alllistedjsfiles.js
@@ -0,0 +1,49 @@
+<script src=
+'https://sureshs2022.w3spaces.com/Airlines_Reservation_System/refunds.js'>
+</script>
+<script src=
+'https://sureshs2022.w3spaces.com/Airlines_Reservation_System/creditdb.js'>
+</script>
+<script src=
+'https://sureshs2022.w3spaces.com/Airlines_Reservation_System/cookies12.js'>
+</script>
+<script src=pay
+'https://sureshs2022.w3spaces.com/Airlines_Reservation_System/customer.js'>
+</script>
+<script src=
+'https://sureshs2022.w3spaces.com/Airlines_Reservation_System/reminder.js'>
+</script>
+<script src=
+'https://sureshs2022.w3spaces.com/Airlines_Reservation_System/datas.js'>
+</script>
+<script src=
+'https://sureshs2022.w3spaces.com/Airlines_Reservation_System/latestticketfare.js'></script>
+<script src=
+'https://github.com/Cool31/Cool31/blob/e9fc07996ada1912b61f7ea3332c6ac628fb5969/bankresponse.js'></script>
+<script src=
+'https://github.com/Cool31/Cool31/blob/d970000d1164e39a4800458712c1d613f70a3eca/paysys.js'>
+</script>
+<script src=
+'https://github.com/Cool31/Ecommerce/blob/6d04b115e67540f7d8f0414749c35e4ee91904bf/storedatas.js'>
+</script>
+<script src=
+'https://github.com/Cool31/Ecommerce/blob/e0e6ad4560307c8387b18e9279bf5997e9fba3a6/cancel.js'>
+</script>
+<script src=
+'https://github.com/Cool31/Ecommerce/blob/997c45f88a5448f33e545c4cbb6b269950bbf2c3/flightchart.js'></script>
+<script src=
+'https://github.com/Cool31/Ecommerce/blob/74e56bcaf604c41123f92e27354b25b03b7c0cdb/formsserver.js'></script>
+<script src=
+'https://github.com/Cool31/Ecommerce/blob/aba268cb46321e39eb18771d1e6f00587723869b/refunds.js'>
+</script>
+<script src=
+'https://github.com/Cool31/Ecommerce/blob/c445447fa30c6249f5372ce5ac14c286cd81c04b/reminder.js'>
+</script>
+<script src=
+'https://github.com/Cool31/Ecommerce/blob/6fe14c2650bd989c40d7ed9b130b21e642cd2e16/open.js'>
+</script>
+<script src=
+'https://github.com/Cool31/Ecommerce/blob/1ded4b29bdc3e93dd330f1c932c9ae44425e6633/postingformdata.js'></script>
+<script src=
+'https://github.com/Cool31/Ecommerce/blob/2a0071179e57146948e81547be6846378c85d44b/virusscan.js'>
+</script>
