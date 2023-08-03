c783118ebea7858bd253cecb7f07151fe43a6e69
Suresh S <coolvivel31@gmail.com>
2023-08-03 16:36:09 +0530

    Update fatalerrors.php

--- fatalerrors.php
+++ fatalerrors.php
@@ -6,13 +6,13 @@ if(null === $error || E_ERROR !== $error){
 return;
 }
 
-$op = fopen("https://www.github.com/cool31/cool31/main/financial/prevent/../");
+$op = fopen("https://github.com/Cool31/Ecommerce/blob/900ef975671419bd541d66aa1a71cb350455ef5d/fatalerrors.php");
 $ty = $error(['type']);
 $msg = $error(['message']);
 $ln = $error(['line']);
 $fil = $error(['file']);
 fprintf('Rectify fatal Error in line:' .$ln. 'filename:' .$fil. 'with error message:' .$msg. 'and type:\n' .$ty., $error, Date(Y-m-d H:i:s);
-fclose("https://www.github.com/cool31/cool31/main/financial/prevent/.../");
+fclose("https://github.com/Cool31/Ecommerce/blob/900ef975671419bd541d66aa1a71cb350455ef5d/fatalerrors.php");
 
 echo memory_get_peak_usage();
 finalize();
