832979f2f80c9bed3814260e74ac443c8022edd2
Suresh S <coolvivel31@gmail.com>
2023-08-03 18:42:14 +0530

    Add files via upload

---
+++ allfilelisted.php
@@ -0,0 +1,48 @@
+<?php include 'https://github.com/Cool31/Ecommerce/blob/5d4fd8c1882dce3fd1e17da51529f531ed8940ea/authorize.php';
+?>
+<?php include  'https://github.com/Cool31/Ecommerce/blob/3dcbe7d80f9685156a3e3abd00255f80b42f961d/customerdatavalidity.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/8fa8419112514b8743cdd0b6ca7b073d0246cc54/flightprint.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/7715ba95446d61bc0a91926f391023813416877c/customervalidation.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/7cd647d5a3b3652f06f021db4726dc3fbe3ecac6/csrf.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/3d6732990f7582f4fdad3acea44aa7ee63804528/curls.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/900ef975671419bd541d66aa1a71cb350455ef5d/fatalerrors.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/91aab211963f3a525137b95c8f25ac0730b0699c/fileformat.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/6b42631a56555c7cedac6793f87d3635e9ece7e0/filechkandcount.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/804b9f4154f365eb46319828c3bb4ec6b5167bfe/fileget.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/dee1b4d458232cfba038431d6e46996be4a9d2c7/fruits.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/4c19aebd84d68279eaad82afa986c311904b6062/ipaddressvalid.php'; ?>
+<?php include
+https://github.com'/Cool31/Ecommerce/blob/16717b6fd972d592eebdd93e7b10ad3a928b17f5/outputdata.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/2c477ac383cd6b830d991e967a914142bd3d2522/phonevalidation.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/f24a2f16e987f8dd4d9d93d85bb3a13b0ac72ef8/phpcodescanner.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/9bfb64d6cf8668dc698a4ab70bc94558f59ddedb/refund.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/027abdf27f167f99d8b993b47cee636e67f9f964/Receipt.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/74eb4c959e6c7c611e1b3707672e536ab51fc875/creditvalidity.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/fe413834636a3b18c5fe9ee4b8b4f4fbec59752a/customervalidation.php'; ?>
+<?php include
+https://github.com'/Cool31/Ecommerce/blob/a98d23b1d587f4bc741dcb04fbb4c3b6bce5f23a/cookph.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/31b6980ff904d14fafe29a76e3f6ff3d3be5b690/rememberme.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/14cec3fa4916efa161427c8eea5ff581c421b730/bamboracreditpay.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/32ae17284d96c9d5824094c65cf43e609771381d/bamboramerchant.php'; ?>
+<?php include
+'https://github.com/Cool31/Ecommerce/blob/74eb4c959e6c7c611e1b3707672e536ab51fc875/creditvalidity.php'; ?>
+# chk customervalidity.php
