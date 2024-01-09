8ce15f25af23bfd64d5b11620f79fc952351c11f
Suresh S <coolvivel31@gmail.com>
2024-01-09 15:33:23 +0530

    Update AddingRecord.php

--- AddingRecord.php
+++ AddingRecord.php
@@ -5,7 +5,7 @@ $db= "db4free123ab";
 $port: 3306;
 
 try{
-Sconn= new PDO('mysql: $dblink, database: $db, username: testing12ab,  $port');
+Sconn= new PDO("mysql: $dblink, database: $db, username: testing12ab,  $port");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn->beginTransaction();
 $stmt->prepare("INSERT INTO fruits(fullname,addr, amt, grandfruit) VALUES(:customer, :shipaddr, :fruitall, :grandtotal)");
